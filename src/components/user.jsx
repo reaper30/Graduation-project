093d9627e77d4d4c58d9d10be1e9ad132adbd6ce
Ilgiz Burganov <vurganowilgiz@gmail.com>
2022-09-10 16:43:37 +0400

    init project

---
+++ src/components/user.jsx
@@ -0,0 +1,83 @@
+import React, {useState} from "react";
+import api from '../api'
+
+const Users = () => {
+  const [users, setUsers ] = useState(api.users.fetchAll())
+  const tableQualityBadge = (item) => {
+    let classes = 'm-2 badge bg-'
+    return  classes+=item
+  }
+
+  const handleDelete = (userId) => {
+    setUsers(prevState => prevState.filter(user => user._id !== userId))
+  }
+
+  const renderPhrase = (number) => {
+   const n = number % 10
+   if (number === 0) {
+     return `Никто с тобой не тусанет`
+   } else if (number >= 11 && number <= 19) {
+      return `${number} Человек тусанет с тобой сегодня`
+   } else if (n === 1 || n === 0 || (n >= 5 && n <= 9)) {
+      return `${number} Человек тусанет с тобой сегодня`
+   } else if (n >= 2 && n <= 4) {
+    return `${number} Человека тусанет с тобой сегодня`
+   }
+  }
+
+  const phraseBadge = (number) => {
+    let classes = 'badge m-2 '
+    return classes += number === 0 ? 'bg-danger' : 'bg-primary' 
+  }
+  
+  return (
+    <>
+      <span className={phraseBadge(users.length)}><h3>{renderPhrase(users.length)}</h3></span>
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Имя</th>
+            <th scope="col">Качества</th>
+            <th scope="col">Профессия</th>
+            <th scope="col">Встретился, раз</th>
+            <th scope="col">Оценка</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user._id}>
+              <th scope="row" >
+                {user.name}
+              </th>
+              <td>
+                {user.qualities.map(quality => (
+                  <span 
+                  className={tableQualityBadge(quality.color)}
+                  key = {quality.color}
+                  >
+                    <h6>{quality.name}</h6>
+                  </span>
+                ))}
+              </td>
+              <td>
+                {user.profession.name} 
+              </td> 
+              <td>
+                {user.completedMeetings}
+              </td>
+              <td>
+                {user.rate}
+              </td>
+              <td>
+                <button type="button" className="btn btn-danger" onClick={() => handleDelete(user._id)}>delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default Users
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Users from './components/user';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Users />
   </React.StrictMode>
 );
 
