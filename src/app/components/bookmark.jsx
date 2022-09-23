83cbfd2fee5f37428270800dbacda1d71c5de7a8
Ilgiz Burganov <vurganowilgiz@gmail.com>
2022-09-24 01:09:35 +0400

    fix 1

--- src/app/components/bookmark.jsx
+++ src/app/components/bookmark.jsx
@@ -1,25 +1,16 @@
 import React from "react";
 
 const BookMark = ({status, id, onBookMark, ...rest}) => {
-  
   return( 
     <>
-      {status === false ? 
-        <button 
-          onClick={() => onBookMark(id)}
-          className="btn btn-dark btn-sm"
-        >
-          <i className="bi bi-bookmark-heart" style={{fontSize: 1.1 + "em"}} ></i>
-        </button>
-        : 
-        <button 
-         onClick={() => onBookMark(id)}
-         className="btn btn-outline-danger btn-sm"
-        >
-          <i className="bi bi-bookmark-heart-fill" style={{fontSize: 1.1 + "em"}} ></i>
-        </button>
-      }
-    </>
+      <button 
+        onClick={() => onBookMark(id)}
+        className={"btn-sm btn btn" + (status ? "-danger" : "-dark")}
+      >
+        <i className={"bi bi-bookmark" + (status ? "-heart-fill" : "-heart")} style={{fontSize: 1.1 + "em"}} ></i>
+      </button>
+   
+    </> 
   )
   
 }
