3430135ed80984c900b912eb994a7f299dff8d85
Ilgiz Burganov <vurganowilgiz@gmail.com>
2022-11-17 18:45:36 +0400

    work with multiSelect 1.1

--- src/app/components/page/editUserPage/editUserPage.jsx
+++ src/app/components/page/editUserPage/editUserPage.jsx
@@ -8,18 +8,45 @@ import PropTypes from "prop-types"
 import { useParams } from "react-router-dom"
 
 const EditUserPage = () => {
-  const [professions, setProfession] = useState({})
+  const getProfessionById = (id) => {
+    for (const prof of professions) {
+      if (prof.value === id) {
+        return { _id: prof.value, name: prof.label }
+      }
+    }
+  }
+  const getQualities = (elements) => {
+    const qualitiesArray = []
+    for (const elem of elements) {
+      //console.log(elem)
+      for (const quality in qualities) {
+        if (elem.value === qualities[quality].value) {
+          qualitiesArray.push({
+            value: qualities[quality].value,
+            label: qualities[quality].label,
+            color: qualities[quality].color
+          })
+        }
+      }
+    }
+    return qualitiesArray
+  }
+
+  const userId = useParams().userId
+
+  const [professions, setProfession] = useState([])
   const [qualities, setQualities] = useState([])
   const [data, setData] = useState({
     name: "",
     email: "",
-    profession: "",
+    profession: getProfessionById(userId),
     sex: "",
-    qualities: []
+    qualities: getQualities(qualities)
   })
 
+  console.log(data.qualities)
   useEffect(() => {
-    api.users.getById(...userId).then((data) => setData(data))
+    api.users.getById(userId).then((data) => setData(data))
     api.professions.fetchAll().then((data) => {
       const professionsList = Object.keys(data).map((professionName) => ({
         label: data[professionName].name,
@@ -36,8 +63,6 @@ const EditUserPage = () => {
       setQualities(qualitiesList)
     })
   }, [])
-  console.log(data)
-  const userId = Object.values(useParams())
 
   const handleChange = (target) => {
     setData((prevState) => ({
@@ -46,57 +71,62 @@ const EditUserPage = () => {
     }))
   }
 
-  return (
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col-md-6 offset-md-3 shadow p-4">
-          <form>
-            <TextField
-              label="Имя"
-              name="name"
-              value={data.name}
-              onChange={handleChange}
-              type="text"
-            />
-            <TextField
-              label="Электронная почта"
-              name="email"
-              type="text"
-              value={data.email}
-              onChange={handleChange}
-            />
-            <SelectField
-              label="Выберите свою профессию"
-              name="profession"
-              value={data.profession._id}
-              onChange={handleChange}
-              options={professions}
-              defaultOption="Choose..."
-            />
-            <RadioField
-              options={[
-                { name: "Male", value: "male" },
-                { name: "Female", value: "female" },
-                { name: "Other", value: "other" }
-              ]}
-              name="sex"
-              value={data.sex}
-              onChange={handleChange}
-              label="Выберите пол"
-            />
-            <MultiSelectField
-              defaultValue={data.qualities}
-              options={qualities}
-              onChange={handleChange}
-              name="qualities"
-              label="Выберите ваши качества"
-            />
-            <button className="btn btn-primary w-100 mx-auto">Обновить</button>
-          </form>
+  if (data.profession) {
+    return (
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 shadow p-4">
+            <form>
+              <TextField
+                label="Имя"
+                name="name"
+                value={data.name}
+                onChange={handleChange}
+                type="text"
+              />
+              <TextField
+                label="Электронная почта"
+                name="email"
+                type="text"
+                value={data.email}
+                onChange={handleChange}
+              />
+              <SelectField
+                label="Выберите свою профессию"
+                name="profession"
+                value={data.profession._id}
+                onChange={handleChange}
+                options={professions}
+                defaultOption="Choose..."
+              />
+              <RadioField
+                options={[
+                  { name: "Male", value: "male" },
+                  { name: "Female", value: "female" },
+                  { name: "Other", value: "other" }
+                ]}
+                name="sex"
+                value={data.sex}
+                onChange={handleChange}
+                label="Выберите пол"
+              />
+              <MultiSelectField
+                defaultValue={data.qualities}
+                options={qualities}
+                onChange={handleChange}
+                name="qualities"
+                label="Выберите ваши качества"
+              />
+              <button className="btn btn-primary w-100 mx-auto">
+                Обновить
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
+  return "Loading..."
 }
 
 EditUserPage.propTypes = {
